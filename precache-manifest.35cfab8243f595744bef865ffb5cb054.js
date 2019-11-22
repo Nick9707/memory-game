d4015baf59a04b69759099ff2b890447750205c6
Nick9707 <kolya.kolombet@gmail.com>
2019-11-22 00:46:03 -0500

    Updates

---
+++ precache-manifest.35cfab8243f595744bef865ffb5cb054.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dca17f59db35eaa3b31b1bf249224cf",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "424f8334bb1c36fa6b28",
+    "url": "/memory-game/static/css/main.07a3d349.chunk.css"
+  },
+  {
+    "revision": "bb0717765918a1cbcc9e",
+    "url": "/memory-game/static/js/2.eccac5df.chunk.js"
+  },
+  {
+    "revision": "424f8334bb1c36fa6b28",
+    "url": "/memory-game/static/js/main.f977ca4a.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  },
+  {
+    "revision": "93ba5e73d40b75ea976331bcbecad482",
+    "url": "/memory-game/static/media/catstello.93ba5e73.png"
+  },
+  {
+    "revision": "92afa446d28da06167309f26053b6190",
+    "url": "/memory-game/static/media/filmtocat.92afa446.png"
+  },
+  {
+    "revision": "473d6fcf25d32649563ab8c8ff2e3548",
+    "url": "/memory-game/static/media/inflatocat.473d6fcf.png"
+  },
+  {
+    "revision": "07745222e22ca29898ac93743f96f8c0",
+    "url": "/memory-game/static/media/justicetocat.07745222.jpg"
+  },
+  {
+    "revision": "bb8eaa1a02d40311f70ddebdf38ada11",
+    "url": "/memory-game/static/media/mcefeeline.bb8eaa1a.jpg"
+  },
+  {
+    "revision": "dd5bd582f6414155568e2baa8beea8e8",
+    "url": "/memory-game/static/media/mountietocat.dd5bd582.png"
+  },
+  {
+    "revision": "a6ea85cafd6dcf7cafdfac02e6121e93",
+    "url": "/memory-game/static/media/scubatocat.a6ea85ca.png"
+  },
+  {
+    "revision": "9a94d2f8ac7722b4974beb78710e13e0",
+    "url": "/memory-game/static/media/vinyltocat.9a94d2f8.png"
+  }
+]);
